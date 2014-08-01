a46755f8b9d5ecefb2f899879aeb3346521a3865
Mick Duan <mick.duan@chinanetcloud.com>
2014-08-01 11:15:03 +0800

    update index

--- controllers/default.go
+++ controllers/default.go
@@ -1,15 +1,15 @@
 package controllers
 
 import (
-	"github.com/astaxie/beego"
+    "github.com/astaxie/beego"
 )
 
 type MainController struct {
-	beego.Controller
+    beego.Controller
 }
 
 func (this *MainController) Get() {
-	this.Data["Website"] = "beego.me"
-	this.Data["Email"] = "astaxie@gmail.com"
-	this.TplNames = "index.tpl"
+    this.Data["Website"] = "doolp.com"
+    this.Data["Email"] = "reidxy@gmail.com"
+    this.TplNames = "index.tpl"
 }
